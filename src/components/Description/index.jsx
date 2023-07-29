11080592403529597663b92c4983f240d85cafb9
Arslaan123Iqbal <arslaniqbalmgt@gmail.con>
2023-07-29 09:58:42 +0500

    Main

---
+++ src/components/Description/index.jsx
@@ -0,0 +1,30 @@
+import styles from './style.module.scss';
+import { useInView, motion } from 'framer-motion';
+import { useRef } from 'react';
+import { slideUp, opacity } from './animation';
+import Rounded from '../../common/RoundedButton';
+export default function index() {
+
+    const phrase = "I am a software engineer with a focus on full-stack development. I have experience working with a range of technologies, including React, Three js , React3Fiber, Python, and Solidity. I am constantly striving to learn and improve my skills, and I am excited to continue growing and developing as a software engineer.";
+    const description = useRef(null);
+    const isInView = useInView(description)
+    return (
+        <div ref={description} className={styles.description}>
+            <div className={styles.body}>
+                <p>
+                {
+                    phrase.split(" ").map( (word, index) => {
+                        return <span className={styles.mask}><motion.span variants={slideUp} custom={index} animate={isInView ? "open" : "closed"} key={index}>{word}</motion.span></span>
+                    })
+                }
+                </p>
+                <motion.p variants={opacity} animate={isInView ? "open" : "closed"}>The combination of my passion for design, code & interaction positions me in a unique place in the Software Engineering world.</motion.p>
+                <div data-scroll data-scroll-speed={0.1}>
+                    <Rounded className={styles.button}>
+                        <p>About me</p>
+                    </Rounded>
+                </div>
+            </div>
+        </div>
+    )
+}
