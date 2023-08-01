b9429921ba802e6ef34729f4d9d50d5633c337e4
Arslaan123Iqbal <arslaniqbalmgt@gmail.con>
2023-08-01 11:22:00 +0500

    finish

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ const inter = Poppins({ subsets: ['latin'] , weight:"400"})
 
 export const metadata = {
   title: 'Arslan Iqbal',
-  description: 'Passionate',
+  description: 'I am a software engineer with a focus on full-stack development. I have experience working with a range of technologies, including React, JavaScript, Python, and Solidity. In my current role, I am responsible for designing and building robust, scalable web applications that deliver a seamless user experience. I am constantly striving to learn and improve my skills, and I am excited to continue growing and developing as a software engineer.',
 }
 
 export default function RootLayout({ children }) {
